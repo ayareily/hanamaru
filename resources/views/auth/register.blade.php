b0001760aaf2d26aedf1c9ce2ba90e7c8772a587
ojou <musiclear05@gmail.com>
2020-03-24 13:01:48 +0900

    auth view edited

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,15 +6,13 @@
   <div class="container">
     <div class="row">
       <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
-        <h1 class="text-center"><a class="text-dark" href="/">memo</a></h1>
+        <h1 class="text-center"><a class="text-dark" href="/">Hanamaru</a></h1>
         <div class="card mt-3">
           <div class="card-body text-center">
             <h2 class="h3 card-title text-center mt-2">ユーザー登録</h2>
 
-            @include('error_card_list')
-
             <div class="card-text">
-              {{--ここから--}}
+
               <form method="POST" action="{{ route('register') }}">
                 @csrf
                 <div class="md-form">
