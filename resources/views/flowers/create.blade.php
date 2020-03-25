eb6b13ff60b18abd1a0fa33d9b1e174dc53b80aa
ojou <musiclear05@gmail.com>
2020-03-25 13:25:51 +0900

    route edit

--- resources/views/flowers/create.blade.php
+++ resources/views/flowers/create.blade.php
@@ -10,10 +10,11 @@
       <div class="col-12">
         <div class="card mt-3">
           <div class="card-body pt-0">
+          <h3>はなまる追加</h3>
             <div class="card-text">
               <form method="POST" action="{{ route('flowers.store') }}">
                 @include('flowers.form')
-                <button type="submit" class="btn blue-gradient btn-block">投稿する</button>
+                <button type="submit" class="btn purple-gradient btn-block">はなまる追加</button>
               </form>
             </div>
           </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 Auth::routes();
+Route::get('/', 'FlowerController@index');
 Route::get('/home', 'FlowerController@index')->name('home');
 Route::get('flowers/create', 'FlowerController@create')->name('flowers.create');
 Route::post('flowers/store', 'FlowerController@store')->name('flowers.store');
