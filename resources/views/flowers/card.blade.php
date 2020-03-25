8bec6ab14f92e7249d8eb5ffc28dec4858fba485
ojou <musiclear05@gmail.com>
2020-03-25 15:14:20 +0900

    card changed

--- resources/views/flowers/card.blade.php
+++ resources/views/flowers/card.blade.php
@@ -1,16 +1,17 @@
 <div class="card mt-3">
-        <div class="card-body d-flex flex-row">
-        <a href="{{ route('home', ['name' => $flower->user->name]) }}" class="text-dark">
-        <i class="fas fa-user-circle fa-3x mr-1"></i>
-        </a>
+          <div class="card-body d-flex flex-row">
           <i class="fas fa-asterisk fa-3x mr-1"></i>
-          <div>
+
+            <div　class="name">
+            <a href="{{ $flower->user->name }}" class="text-dark">
+                {{ $flower->user->name }}
+            
             <div class="font-weight-lighter">
               {{ $flower->created_at->format('Y/m/d H:i') }} 
-          </div>
-        </div>
+            </div>
+        
           <div class="body">
             {{ $flower->body }} 
           </div>
         </div>
-      </div>
+</div>
