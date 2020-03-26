7f85a7b678dbabef1b89927e85eb5f3d592bf5c3
ojou <musiclear05@gmail.com>
2020-03-26 13:06:58 +0900

    blade improvement

--- resources/views/flowers/create.blade.php
+++ resources/views/flowers/create.blade.php
@@ -7,10 +7,11 @@
 @section('content')
   <div class="container">
     <div class="row">
-      <div class="col-12">
+    <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
+        <h1 class="text-center"></h1>
         <div class="card mt-3">
-          <div class="card-body pt-0">
-          <h3>はなまる追加</h3>
+          <div class="card-body text-center">
+            <h2 class="h3 card-title text-center mt-2">はなまる追加</h2>
             <div class="card-text">
               <form method="POST" action="{{ route('flowers.store') }}">
                 @include('flowers.form')
