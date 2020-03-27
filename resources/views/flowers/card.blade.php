1b99950b8faa653cd301258821d1b36948f883c5
ojou <musiclear05@gmail.com>
2020-03-27 16:46:48 +0900

    migration edit

--- resources/views/flowers/card.blade.php
+++ resources/views/flowers/card.blade.php
@@ -1,4 +1,6 @@
 <div class="card mt-3">
+  @if( Auth::id() === $flower->user_id )
+
           <div class="card-body d-flex flex-row">
           <i class="fas fa-asterisk fa-3x mr-1"></i>
           <div>
@@ -15,8 +17,6 @@
             </div>
             </div>
 
-
-  @if( Auth::id() === $flower->user_id )
     <!-- dropdown -->
       <div class="ml-auto card-text float-right">
         <div class="dropdown">
@@ -60,7 +60,6 @@
         </div>
       </div>
       <!-- modal -->
-    @endif
 
 </div>
     <div class="card-body pt-0">        
@@ -68,5 +67,6 @@
             {{ $flower->body }} 
     </div>
     </div>
+    @endif
 
 </div>
