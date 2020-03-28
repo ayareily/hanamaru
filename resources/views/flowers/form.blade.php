a877f1a32784525f1e29c347201698016a6e85e5
ojou <musiclear05@gmail.com>
2020-03-28 14:13:39 +0900

    補完機能追加

--- resources/views/flowers/form.blade.php
+++ resources/views/flowers/form.blade.php
@@ -2,6 +2,11 @@
 
 <div class="form-group">
   <label></label>
-  <textarea name="foruser" class="form-control" rows="1" placeholder="送信先">{{ $flower->foruser ?? old('id') }}</textarea>
+  <div class="form-group">
+  <article-tags-input
+  >
+  </article-tags-input>
+</div>
+
   <textarea name="body" class="form-control" rows="3" placeholder="はなまるメモ">{{ $flower->body ?? old('id') }}</textarea>
 </div>
